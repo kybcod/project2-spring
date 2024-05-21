1832355f41f6afd8e0a56bfbda0633e3ea724c81
kyb <kyb-0215@naver.com>
2024-05-21 11:50:55 +0900

    글 작성 board 값 받아오기

---
+++ src/main/java/com/project2spring/controller/domain/board/Board.java
@@ -0,0 +1,10 @@
+package com.project2spring.controller.domain.board;
+
+import lombok.Data;
+
+@Data
+public class Board {
+    private String title;
+    private String content;
+    private String writer;
+}
