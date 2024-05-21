8c27d24a88eb077d10ca451acb3a830d2b517059
kyb <kyb-0215@naver.com>
2024-05-21 10:12:38 +0900

    first commit

---
+++ src/main/java/com/project2spring/Project2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.project2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project2SpringApplication.class, args);
+    }
+
+}
