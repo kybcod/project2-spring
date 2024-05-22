eae6b4603df6ee60c52b0d67fc227d53ea6e0af4
kyb <kyb-0215@naver.com>
2024-05-22 09:11:35 +0900

    게시글 추가 서버 오류

--- src/main/java/com/project2spring/controller/domain/board/Board.java
+++ src/main/java/com/project2spring/controller/domain/board/Board.java
@@ -2,9 +2,13 @@ package com.project2spring.controller.domain.board;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class Board {
+    private Integer id;
     private String title;
     private String content;
     private String writer;
+    private LocalDateTime inserted;
 }
--- src/main/java/com/project2spring/mapper/board/BoardMapper.java
+++ src/main/java/com/project2spring/mapper/board/BoardMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface BoardMapper {
     @Insert("""
-            INSERT INTO board
+            INSERT INTO board (title, content, writer)
             VALUES (#{title}, #{content}, #{writer})
             """)
     int insert(Board board);
