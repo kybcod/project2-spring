d90a801962e0b89acc15d72878512f101f485c0a
kyb <kyb-0215@naver.com>
2024-05-28 10:44:15 +0900

    파일을 disk에 저장

--- src/main/java/com/project2spring/controller/board/BoardController.java
+++ src/main/java/com/project2spring/controller/board/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -22,7 +23,7 @@ public class BoardController {
     @PostMapping("add")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity add(Authentication authentication,
-                              Board board, @RequestParam(value = "files[]", required = false) MultipartFile[] files) throws InterruptedException {
+                              Board board, @RequestParam(value = "files[]", required = false) MultipartFile[] files) throws IOException {
 
         if (service.validate(board)) {
             service.add(board, files, authentication);
